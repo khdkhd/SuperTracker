27f82d25c869b79f96430ae0d0aae00fc96b0d57
NealRame <contact@nealrame.com>
2017-09-29 22:43:26 +0200

    Initial import

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	root: true,
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module'
+	},
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: 'standard',
+	globals: {
+		__static: true
+	},
+	plugins: ['html'],
+	'rules': {
+		'arrow-parens': 0, // allow paren-less arrow functions
+		'comma-dangle': ['warn', 'always-multiline'],
+		'generator-star-spacing': 0, // allow async-await
+		'global-require': 0,
+		'import/newline-after-import': 0,
+		'import/no-unresolved': 0,
+		'import/prefer-default-export': 0,
+		'indent': ['error', 'tab'],
+		'no-multi-assign': 0,
+		'no-shadow': 0,
+		'no-tabs': 0,
+		'space-before-function-paren': 0,
+		'space-infix-ops': 0,
+		// allow debugger during development
+		'no-debugger': process.env.NODE_ENV === 'production'
+			? 2
+			: 0
+	}
+}
---
+++ src/renderer/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+<div id="wrapper">
+</div>
+</template>
+
+<script>
+export default {
+	name: 'landing-page',
+	components: {
+	},
+	methods: {
+	},
+}
+</script>
+
+<style>
+</style>
