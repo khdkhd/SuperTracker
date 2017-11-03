c101ef0b0d16ca007a82b6fe1cbab542add239d7
NealRame <contact@nealrame.com>
2017-11-03 23:14:12 +0100

    Update ButtonGroup test

--- src/renderer/components/Tests/ButtonGroup.vue
+++ src/renderer/components/Tests/ButtonGroup.vue
@@ -1,38 +1,41 @@
 <template>
 	<div>
 		<span>foo</span>
-		<button-group id="action" :buttons="buttons"></button-group>
+		<button-group id="action" :buttons="buttons" @click="onClick"></button-group>
 		<span>foo</span>
 	</div>
 </template>
 
 <script>
 import ButtonGroup from '@/renderer/components/ButtonGroup'
+import random from 'lodash.random'
+import {times} from 'ramda'
+
+const letters = 'abcdefghijklmnopqrstuvwxyz'
+function generateId() {
+	return times(() => letters[random(0, letters.length - 1)], 4).join('')
+}
 
 export default {
 	name: 'ButtonGroup-test',
 	data() {
 		return {
-			buttons: [
-				{
-					id: 'play',
-				},
-				{
-					id: 'loop',
-				},
-				{
-					id: 'stop',
-				},
-				{
-					id: 'edit',
-				},
-			],
+			buttons: [{
+				id: generateId(),
+			}],
 		}
 	},
 	components: {
 		ButtonGroup,
 	},
 	methods: {
+		onClick(...args) {
+			if (this.buttons.length < 16) {
+				this.buttons.push({
+					id: generateId(),
+				})
+			}
+		},
 	},
 }
 </script>
