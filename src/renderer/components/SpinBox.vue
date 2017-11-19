f253502ffb9598094840a4502a3b8cee2a1cd672
NealRame <contact@nealrame.com>
2017-11-19 19:46:05 +0100

    Spinbox component

---
+++ src/renderer/components/SpinBox.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="spin-box">
+		<input
+			ref="input"
+			type="number"
+			name=""
+			@input="onValueChanged"
+			:min="min"
+			:max="max"
+			:step="step"
+			:value="initValue">
+		<div class="spinbox-arrow-wrapper">
+			<button ref="up" class="spinbox-arrow" data-action="up" @click="onArrowClicked"/>
+			<button ref="down" class="spinbox-arrow" data-action="down" @click="onArrowClicked"/>
+		</div>
+	</div>
+</template>
+
+<script>
+import { clamp, defaultTo } from 'ramda'
+import '@/renderer/style/spinbox.scss'
+
+const defaultMin = -Infinity
+const defaultMax = Infinity
+const defaultVal = 0
+const defaultPrecision = 2
+
+export default {
+	name: 'spin-box',
+	components: {
+	},
+	props: ['min', 'max', 'precision', 'step', 'value'],
+	methods: {
+		currentValue() {
+			return Number(this.$refs.input.value)
+		},
+		onValueChanged(ev) {
+			this.$emit('value-changed', Number(ev.target.value))
+		},
+		onArrowClicked({ target }) {
+			const sign = target === this.$refs.up ? 1 : -1
+			const incr = sign*Number(this.step)
+			const precision = defaultTo(defaultPrecision, this.precision)
+			const value = Number((this.currentValue() + incr).toFixed(precision))
+			this.$refs.input.value = value
+		},
+	},
+	computed: {
+		initValue() {
+			return clamp(
+				Number(defaultTo(defaultMin, this.min)),
+				Number(defaultTo(defaultMax, this.max)),
+				Number(defaultTo(defaultVal, this.value))
+			)
+		},
+	},
+}
+</script>
---
+++ src/renderer/components/Tests/SpinBox.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<span>foo</span>
+		<spin-box min=0 max=4 step=.01 value=2 @value-changed="onValueChanged"></spin-box>
+		<button></button>
+	</div>
+</template>
+
+<script>
+import SpinBox from '@/renderer/components/SpinBox'
+
+export default {
+	name: 'ButtonGroup-test',
+	data() {
+		return {
+		}
+	},
+	components: {
+		SpinBox,
+	},
+	methods: {
+		onValueChanged(value) {
+			console.log(value)
+		},
+	},
+}
+</script>
