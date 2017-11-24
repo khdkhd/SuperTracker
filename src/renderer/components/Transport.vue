4b0b1448b19b8876f024d794d9de36f92a4e086c
NealRame <contact@nealrame.com>
2017-11-25 00:09:17 +0100

    Transport refactoring

--- src/renderer/components/Transport.vue
+++ src/renderer/components/Transport.vue
@@ -1,33 +1,34 @@
 <template>
-<div id="transport">
-	<ul>
-		<li>
-			<button-group
-				id="action"
-				ref="action"
-				:buttons="buttons"
-				@clicked="onActionButtonClicked">
-			</button-group>
-		</li>
-		<li class="separator"></li>
-		<li>
-			<spin-box
-				id="tempo"
-				ref="tempo"
-				min=30
-				max=300
-				step=.01
-				value=120
-				@value-changed="onTempoValueChanged">
-			</spin-box>
-		</li>
-	</ul>
-</div>
+<ul id="transport">
+	<li>
+		<button-group
+			id="action"
+			ref="action"
+			:buttons="buttons"
+			@clicked="onActionButtonClicked">
+		</button-group>
+	</li>
+	<li class="separator"></li>
+	<li>
+		<span>BPM</span>
+		<spin-box
+			id="tempo"
+			ref="tempo"
+			:min="tempoMin"
+			:max="tempoMax"
+			:step="tempoStep"
+			@value-changed="onTempoValueChanged">
+		</spin-box>
+	</li>
+</ul>
 </template>
 
 <script>
 import ButtonGroup from '@/renderer/components/ButtonGroup'
 import SpinBox from '@/renderer/components/SpinBox'
+
+import '@/renderer/style/transport.scss'
+
 export default {
 	name: 'transport',
 	components: {
@@ -36,6 +37,9 @@ export default {
 	},
 	data() {
 		return {
+			tempoMax: 300,
+			tempoMin: 30,
+			tempoStep: 0.01,
 			buttons: [
 				{
 					id: 'play',
@@ -62,52 +66,3 @@ export default {
 	},
 }
 </script>
-
-<style lang="scss">
-$transport-bar-height: 1.5rem;
-$item-separator-color: lighten($app-bg-color, 10);
-$item-spacing: .03125rem;
-$item-spacing: 0;
-$item-margin: 0;
-$item-margin: .25rem;
-
-#transport {
-	#action button {
-		&#play::after {
-			content: '\f04b';
-		}
-		&#stop::after {
-			content: '\f04d';
-		}
-		&#loop::after {
-			content: '\f01e';
-		}
-		&#edit::after {
-			content: '\f111';
-		}
-		&#pattern-follow-mode::after {
-			content: '\f175';
-		}
-	}
-	ul {
-		height: $transport-bar-height;
-		list-style-type: none;
-		margin: 0;
-		padding: 0;
-		li {
-			display: inline-block;
-			line-height: $transport-bar-height;
-			height: 100%;
-			margin: $item-spacing 0;
-			&.separator {
-				border: none;
-				border-left: 1px solid $item-separator-color;
-				vertical-align: middle;
-				&::after {
-					content: ' ';
-				}
-			}
-		}
-	}
-}
-</style>
