b1be9152a81960be359fcd88dcf698a0443dde9c
NealRame <contact@nealrame.com>
2017-12-01 23:43:02 +0100

    Add pattern

--- src/renderer/components/Main.vue
+++ src/renderer/components/Main.vue
@@ -1,26 +1,39 @@
 <template>
 <div id="main-wrapper">
-	<transport></transport>
-	<!-- <visualisation></visualisation>
-	<tracks></tracks> -->
+	<st-transport></st-transport>
+	<div style="text-align: center">
+		<!-- <st-visualisation></st-visualisation> -->
+		<st-pattern :length="length" :tracks="tracks"></st-pattern>
+	</div>
 </div>
 </template>
 
 <script>
-import Tracks from '@/renderer/components/Tracks'
+import Pattern from '@/renderer/components/Pattern'
 import Transport from '@/renderer/components/Transport'
 import Visualisation from '@/renderer/components/Visualisation'
+
+import {times} from 'ramda'
+
+const length = 64
+const trackCount = 8
 
 export default {
 	name: 'main',
 	data() {
 		return {
+			length,
+			tracks: times(index => ({
+				name: `Track ${index.toFixed(0).padStart(2, '0')}`,
+				steps: times(index => ({isActive: false}), length),
+				isActive: index === 0,
+			}), trackCount),
 		}
 	},
 	components: {
-		Tracks,
-		Transport,
-		Visualisation,
+		'st-pattern': Pattern,
+		'st-transport': Transport,
+		'st-visualisation': Visualisation,
 	},
 	methods: {
 	},
